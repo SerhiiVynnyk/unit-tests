8671f73fd8ee8c7e13ed7a0544fd5116ee5ab081
= <=>
2022-02-14 12:26:14 +0200

    change text

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -1,5 +1,5 @@
 import { Calculator } from "../src/calc";
-describe("test add function", () => {
+describe("test Calculator class", () => {
     let calc1 = new Calculator(4,5);
   it("should return 9 for add(4,5)", () => {
     expect(calc1.calculate().sum1).toBe(9);
