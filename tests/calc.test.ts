af9ab15dc4144e1a2e6ba6bd092db16c6bdcfba2
= <=>
2022-02-14 15:50:01 +0200

    text mistake

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -4,13 +4,13 @@ describe("test Calculator class", () => {
  it("should return 9 for add(4,5)", () => {
     expect(calc.calculate(4,5,"+")).toBe(9);
   }); 
-  it("should return 9 for difference(4,5)", () => {
+  it("should return -1 for difference(4,5)", () => {
     expect(calc.calculate(4,5,"-")).toBe(-1);
   });
-  it("should return 9 for division(4,5)", () => {
+  it("should return 0.8 for division(4,5)", () => {
     expect(calc.calculate(4,5,"/")).toBe(0.8);
   });
-  it("should return 9 for multiple(4,5)", () => {
+  it("should return 20 for multiple(4,5)", () => {
     expect(calc.calculate(4,5,"*")).toBe(20);
   });
 });
